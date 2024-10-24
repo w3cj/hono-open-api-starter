92525ff84fb2a247c8245cc889b2320d7b3b6e2c
w3cj <cj@null.computer>
2024-10-24 15:26:47 -0600

    chore: upgrade drizzle to v0.35

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,16 +2,16 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const tasks = sqliteTable("tasks", {
-  id: integer("id", { mode: "number" })
+  id: integer({ mode: "number" })
     .primaryKey({ autoIncrement: true }),
-  name: text("name")
+  name: text()
     .notNull(),
-  done: integer("done", { mode: "boolean" })
+  done: integer({ mode: "boolean" })
     .notNull()
     .default(false),
-  createdAt: integer("created_at", { mode: "timestamp" })
+  createdAt: integer({ mode: "timestamp" })
     .$defaultFn(() => new Date()),
-  updatedAt: integer("updated_at", { mode: "timestamp" })
+  updatedAt: integer({ mode: "timestamp" })
     .$defaultFn(() => new Date())
     .$onUpdate(() => new Date()),
 });
