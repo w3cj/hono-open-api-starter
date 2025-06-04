8c89fcd392e522e8a48e5c21422da329d3a70a86
w3cj <cj@null.computer>
2025-06-04 08:44:21 -0600

    :sparkles: :package: upgrade dependencies

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,4 +1,4 @@
-import { apiReference } from "@scalar/hono-api-reference";
+import { Scalar } from "@scalar/hono-api-reference";
 
 import type { AppOpenAPI } from "./types";
 
@@ -15,15 +15,13 @@ export default function configureOpenAPI(app: AppOpenAPI) {
 
   app.get(
     "/reference",
-    apiReference({
+    Scalar({
+      url: "/doc",
       theme: "kepler",
       layout: "classic",
       defaultHttpClient: {
         targetKey: "js",
         clientKey: "fetch",
-      },
-      spec: {
-        url: "/doc",
       },
     }),
   );
